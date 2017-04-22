40101d57a986397cbfec8898df6c9250e31b7739
DESKTOP-F4HLLCB\danil <moehbioshk@gmail.com>
2017-04-22 17:57:58 -0300

    +++_
    +++

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class ThrowBall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public static ThrowBall instance;
+
+    public EnemyBullet bullet;
+
+    private List<EnemyBullet> bullets;
+
+    [SerializeField]
+    private float randTimeMin;
+    [SerializeField]
+    private float randTimeMax;
+
+    private float timeToShot;
+    private int currentBullet = 0;
+    
+    // Use this for initialization
+    void Start () {
+
+        instance = this;
+
+        timeToShot = Random.Range(randTimeMin, randTimeMax);
+        bullets = new List<EnemyBullet>();
+        for(int i = 0; i < 5; i++) {
+            EnemyBullet obj = Instantiate(bullet, transform.Find("tube")) as EnemyBullet;
+            bullets.Add(obj);
+            obj.gameObject.SetActive(false);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        timeToShot -= Time.deltaTime;
+        //if(timeToShot <= 0.2f && timeToShot > 0)
+        transform.LookAt(Camera.main.transform);
+        if (timeToShot <= 0) {
+            if(currentBullet < bullets.Count) {
+                bullets[currentBullet].gameObject.SetActive(true);
+                bullets[currentBullet].shot(20);
+                currentBullet++;
+            }
+            timeToShot = Random.Range(randTimeMin, randTimeMax);
+        }
 	}
+
+    public void resetBullet(EnemyBullet b) {
+        b.transform.parent = this.transform;
+        b.transform.localRotation = Quaternion.identity;
+        b.transform.localPosition = Vector3.zero;
+        b.gameObject.SetActive(false);
+        currentBullet--;
+    }
 }
