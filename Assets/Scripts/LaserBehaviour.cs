82f2f22f31bac839b05f93f25f960661468dd9dd
Vívio <vivio_pgmr@yahoo.com>
2017-04-23 10:11:00 -0300

    Criados scripts relacionados ao controle do player.

---
+++ Assets/Scripts/LaserBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5.0f;
+
+	void Start () {}
+	
+	void Update () {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private int health = 10;
+
+    public GameObject bullet;
+
+    private GestureRecognizer recognizer;
+
+    void Start () {
+        recognizer = new GestureRecognizer();
+
+        recognizer.TappedEvent += (source, tapCount, ray) => {
+            if (bullet != null) {
+                Instantiate(bullet, transform.position + transform.forward * 0.25f, transform.rotation);
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Update () {}
+
+    void TakeDamage(int damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            // END GAME!
+        }
+    }
+}
